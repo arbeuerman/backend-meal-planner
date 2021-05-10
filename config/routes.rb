1223dfb363199ac8a9edb2b63ac4643ee7337b5c
Alexandra <arbeuerman@gmail.com>
2021-05-10 16:04:48 -0500

    making url route more clear

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :meals
   resources :recipe_ingredients
   # resources :recipes, only: [:index]
-  get '/recipes/:limit/:offset', to: 'recipes#index', as: 'recipes'
+  get '/recipes/limit=:limit/offset=:offset', to: 'recipes#index', as: 'recipes'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
