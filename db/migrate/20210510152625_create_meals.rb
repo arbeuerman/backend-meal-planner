cbfa9bec0a964aed620ef5d2bd9a0fd62fc7a358
Alexandra <arbeuerman@gmail.com>
2021-05-10 10:29:04 -0500

    created models

---
+++ db/migrate/20210510152625_create_meals.rb
@@ -0,0 +1,11 @@
+class CreateMeals < ActiveRecord::Migration[6.1]
+  def change
+    create_table :meals do |t|
+      t.string :typeOfMeal
+      t.belongs_to :recipeingredients, null: false, foreign_key: true
+      t.string :date
+
+      t.timestamps
+    end
+  end
+end
