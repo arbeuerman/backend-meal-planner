520adf41b46fee9071544a933662e7cd6e36955d
Alexandra <arbeuerman@gmail.com>
2021-05-12 13:20:57 -0500

    updating urls for meals

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  
   # resources :meals
   get '/meals', to: 'meals#index', as: 'meals'
   patch '/meals/:id', to: 'meals#update', as: 'meal'
+  
   resources :recipe_ingredients
+  
   # resources :recipes, only: [:index]
   get '/recipes/limit=:limit/offset=:offset', to: 'recipes#index', as: 'recipes'
   get '/recipes/:id', to: 'recipes#show', as: 'recipe'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
