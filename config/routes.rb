498c057cff565d4fc990aa42566ce412f9863407
Alexandra <arbeuerman@gmail.com>
2021-05-12 14:49:18 -0500

    changed spacing in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  
   # resources :meals
   get '/meals', to: 'meals#index', as: 'meals'
   patch '/meals/:id', to: 'meals#update', as: 'meal'
   delete '/meals/:id', to: 'meals#destroy'
+  
   resources :recipe_ingredients
   
   # resources :recipes, only: [:index]
